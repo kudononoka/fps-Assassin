ea93e2fc04c9261fc94fa12fb7bac7ba006a3a14
Otsu01 <kudo.nonoka02@gmail.com>
2022-11-16 18:45:41 +0900

    IK入れてみた！右手と左手のアニメーション制御　銃を持っているようにみせた

---
+++ Assets/Scripts/HandIK.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandIK : MonoBehaviour
+{
+    
+    [SerializeField,Header("右手を置く場所")] Transform _rightTarget;
+    [SerializeField,Header("左手を置く場所")] Transform _leftTarget;
+    /// <summary>右手の Position に対するウェイト</summary>
+    [SerializeField, Range(0f, 1f)] float _rightPosWeight = 0;
+    /// <summary>右手の Rotation に対するウェイト</summary>
+    [SerializeField, Range(0f, 1f)] float _rightRotWeight = 0;
+    /// <summary>左手の Position に対するウェイト</summary>
+    [SerializeField, Range(0f, 1f)] float _leftPosWeight = 0;
+    /// <summary>左手の Rotation に対するウェイト</summary>
+    [SerializeField, Range(0f, 1f)] float _leftRotWeight = 0;
+    Animator _anim;
+
+    void Start()
+    {
+        _anim = GetComponent<Animator>();
+    }
+
+    void OnAnimatorIK(int layerIndex)
+    {
+        // 右手
+        _anim.SetIKPositionWeight(AvatarIKGoal.RightHand, _rightPosWeight);
+        _anim.SetIKRotationWeight(AvatarIKGoal.RightHand, _rightRotWeight);
+        _anim.SetIKPosition(AvatarIKGoal.RightHand, _rightTarget.position);
+        _anim.SetIKRotation(AvatarIKGoal.RightHand, _rightTarget.rotation);
+        // 左手
+        _anim.SetIKPositionWeight(AvatarIKGoal.LeftHand, _leftPosWeight);
+        _anim.SetIKRotationWeight(AvatarIKGoal.LeftHand, _leftRotWeight);
+        _anim.SetIKPosition(AvatarIKGoal.LeftHand, _leftTarget.position);
+        _anim.SetIKRotation(AvatarIKGoal.LeftHand, _leftTarget.rotation);
+    }
+}
---
+++ Assets/Scripts/Player/PlayerMove.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    Rigidbody _rb;
+    Animator _anim;
+    [Tooltip("カメラのY軸方向の角度を取得する変数")] Quaternion _forward;
+    /// <summary>MainCamera</summary>
+    GameObject _mainCamera;
+
+    /// <summary>VirtualCamera 三人称視点</summary>
+    [Tooltip("主要カメラ")] GameObject _cmMain;
+
+    /// <summary> VirtualCamera 三人称視点　プレイヤー近くの背後の画面</summary>
+    [Tooltip("ズーム時のカメラ")] GameObject _cmSub;
+
+    //[Tooltip("ズーム時の照準UI")] Image _target;
+    //[Tooltip("普段時の照準UI")] Image _normal;
+
+    Quaternion upperFoward;
+
+    
+    
+    [SerializeField, Header("移動速度"), Tooltip("移動速度")] float _runSpeed = 7f;
+    float _rotateSpeed = 600;
+    Quaternion _rotationTo = Quaternion.identity;
+    Quaternion _mainCamaraforward = Quaternion.identity;
+    bool isSet = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rb = GetComponent<Rigidbody>();
+        _anim = GetComponent<Animator>();
+
+        _mainCamera = GameObject.Find("Main Camera");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = Input.GetAxisRaw("Horizontal");
+        float z = Input.GetAxisRaw("Vertical");
+
+        //カメラのy軸のオイラー角を取得
+        _mainCamaraforward = Quaternion.AngleAxis(_mainCamera.transform.eulerAngles.y, Vector3.up);
+        _rb.velocity = _mainCamaraforward.normalized * new Vector3(x * _runSpeed, 0, -z * _runSpeed);
+
+        if (_rb.velocity.magnitude >= 1)
+        {
+            _rotationTo = Quaternion.LookRotation(_rb.velocity, Vector3.up);
+        }
+
+        //滑らかに回転させる
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, _rotationTo, _rotateSpeed * Time.deltaTime);
+
+
+        _anim.SetFloat("run", _rb.velocity.magnitude);
+    }
+}
